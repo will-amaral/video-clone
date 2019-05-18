f545b8be9dab50de3786200481128b809fdc65c0
Willian Amaral <willian.amaral@outlook.com.br>
2019-05-17 21:52:44 -0300

    Updates

---
+++ precache-manifest.7ac0a39bb9cbcdc3363cd92a088045c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e522ff45cd9a0e369668e5896c6efaaa",
+    "url": "/video-clone/index.html"
+  },
+  {
+    "revision": "a8b382ed7ca15c61661c",
+    "url": "/video-clone/static/css/main.0f1aed7e.chunk.css"
+  },
+  {
+    "revision": "5db97938c6f395e14e57",
+    "url": "/video-clone/static/js/2.34b6a278.chunk.js"
+  },
+  {
+    "revision": "a8b382ed7ca15c61661c",
+    "url": "/video-clone/static/js/main.3244ebd5.chunk.js"
+  },
+  {
+    "revision": "4d4f3405e3a383238dbe",
+    "url": "/video-clone/static/js/runtime~main.a5962bf7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/will-amaral/precache-manifest.12b8a19536cfd63ed016d4ea011bfbdb.js"
+  "/video-clone/precache-manifest.7ac0a39bb9cbcdc3363cd92a088045c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/will-amaral/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/video-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
