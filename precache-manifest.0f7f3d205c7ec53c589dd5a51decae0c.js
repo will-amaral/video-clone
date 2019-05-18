ceceb25bda9d333ae9bfc4c443d81885f19121e6
Willian Amaral <willian.amaral@outlook.com.br>
2019-05-17 21:37:21 -0300

    Updates

---
+++ precache-manifest.0f7f3d205c7ec53c589dd5a51decae0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e302168fa93c85efc4b6cf6bfec59c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d0b83865fcbaf6c33ba",
+    "url": "/static/css/main.0f1aed7e.chunk.css"
+  },
+  {
+    "revision": "5db97938c6f395e14e57",
+    "url": "/static/js/2.34b6a278.chunk.js"
+  },
+  {
+    "revision": "0d0b83865fcbaf6c33ba",
+    "url": "/static/js/main.646ccf08.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
